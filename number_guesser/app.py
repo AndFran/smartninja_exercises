86307503a00c4a786866798d21fe33b694ad2285
AndFran <andrewwilliamfranks@gmail.com>
2018-05-31 10:55:47 +0200

    number guesser first commit

---
+++ number_guesser/app.py
@@ -0,0 +1,27 @@
+from random import randint
+
+
+def main():
+    secret = randint(1, 100)
+
+    while True:
+        print(secret)
+        guess = raw_input("Enter a number between 1 and 99 (q to quit):")
+        guess = guess.lower()
+        if guess == 'q':
+            exit(0)
+        try:
+            guess = int(guess)
+        except ValueError:
+            print "Enter a valid number or q to quit"
+        if guess == secret:
+            print "YOU WIN"
+            exit(0)
+        elif guess > secret:
+            print "You guess is too high"
+        else:
+            print "You guess is too low"
+
+
+if __name__ == '__main__':
+    main()
