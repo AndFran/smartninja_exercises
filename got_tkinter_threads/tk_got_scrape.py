3e8f1d776306a7bd6c419e2c59ec3314d86f2c38
AndFran <andrewwilliamfranks@gmail.com>
2018-06-26 14:13:41 +0200

    file and about dialog and ....
    Added file and about dialog. Also changed the download button to be an image.

--- got_tkinter_threads/tk_got_scrape.py
+++ got_tkinter_threads/tk_got_scrape.py
@@ -1,6 +1,8 @@
 import Tkinter as tk
 import random
 import threading
+import tkMessageBox
+import tkFileDialog
 
 from got_fetcher import get_seasons, get_episode_data
 from colors_got import all_colors
@@ -10,14 +12,27 @@ class GOT_Download_GUI:
     def __init__(self, master):
         self.master = master
         master.title("GOT downloader")
-        self.logo = tk.PhotoImage(file="got.gif")
-        self.label = tk.Label(master, image=self.logo)
-        self.label.pack()
+        master.configure(background='aquamarine')
+
+        menu_bar = tk.Menu(master)
+        file_menu = tk.Menu(menu_bar, tearoff=0)
+        file_menu.add_command(label="Save", command=self.save_results)
+        file_menu.add_separator()
+        file_menu.add_command(label="Exit", command=master.quit)
+        menu_bar.add_cascade(label="File", menu=file_menu)
+
+        help_menu = tk.Menu(menu_bar, tearoff=0)
+        help_menu.add_command(label="About", command=self.about)
+        menu_bar.add_cascade(label="Help", menu=help_menu)
+
+        master.config(menu=menu_bar)
 
         self.download_button = tk.Button(master, text="download data", command=self.get_got_data)
+        self.logo = tk.PhotoImage(file="got.gif")
+        self.download_button.config(image=self.logo, activebackground="black", bg="black", bd=0)
         self.download_button.pack()
 
-        self.results_text = tk.Text(root, height=12, width=50)
+        self.results_text = tk.Text(root, state='disabled', height=12, width=50)
         self.results_text.pack()
 
         self.cancel_button = tk.Button(master, text="Cancel download", command=self.cancel_download)
@@ -29,6 +44,18 @@ class GOT_Download_GUI:
         self.canvas = tk.Canvas(master, width=self.canvas_width, height=self.canvas_height)
         self.canvas.pack()
         self.stop_thread = False
+
+    def save_results(self):
+        f = tkFileDialog.asksaveasfile(mode='w', initialfile="got_download_data", defaultextension=".txt")
+        if f is None:
+            return
+        text_to_write = str(self.results_text.get(1.0, tk.END))
+        f.write(text_to_write)
+        f.close()
+
+    def about(self):
+        text = "A GUI app to web scrape wikipedia for Game of Thrones view stats"
+        tkMessageBox.showinfo("About", text)
 
     def draw_lines(self, seasons):
         size = 40
@@ -56,11 +83,16 @@ class GOT_Download_GUI:
         self.download_button.config(state=tk.DISABLED)
         self.cancel_button.config(state="normal")
 
-    def get_got_data(self):
+    def write_results_text(self, text):
+        self.results_text.configure(state='normal')
+        self.results_text.delete('1.0', tk.END)
+        self.results_text.insert(tk.END, text)
+        self.results_text.configure(state='disabled')
 
+    def get_got_data(self):
         self._set_allow_cancel_state()
         # clear gui
-        self.results_text.delete(1.0, tk.END)
+        self.write_results_text("")
         self.canvas.delete("all")
 
         def callback():
@@ -79,12 +111,12 @@ class GOT_Download_GUI:
                 running_total += s.season_total
 
             if self.stop_thread:
-                self.results_text.insert(tk.END, "Download Aborted")
+                self.write_results_text("Download Aborted")
                 self.stop_thread = False
             else:
                 text += "Overall total: " + str(running_total)
                 self.draw_lines(seasons)
-                self.results_text.insert(tk.END, text)
+                self.write_results_text(text)
 
             self._set_allow_download_state()
 
